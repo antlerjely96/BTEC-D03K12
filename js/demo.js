3a94b7e524684c7deaca5c21c1e39fe77617aa04
antlerjely96 <antlerjely96@gmail.com>
2022-02-19 15:33:28 +0700

    add BT buoi 12

---
+++ js/demo.js
@@ -0,0 +1,5 @@
+function demo(){
+    document.getElementById("the_p").innerHTML = "Đây không phải thẻ p";
+    document.getElementById("the_p").title = "Đây là title";
+    document.getElementById("the_p").style.color = "red";
+}
